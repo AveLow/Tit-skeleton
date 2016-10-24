db6e810ddcc364e32c3d675e83a59d7a5c479a60
Alexis Merceron <merceron.alexis.dev@gmail.com>
2016-10-24 18:45:34 -0400

    Add Acme example
    AcmeController
    index view
    update config file

---
+++ src/Acme/Controller/AcmeController.php
@@ -0,0 +1,27 @@
+<?php
+namespace src\Acme\Controller;
+
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Tit\lib\Components\Controller;
+
+/**
+ * Class AcmeController
+ * @package src\Acme\Controller
+ *
+ * Exemple Controller to show how it works.
+ */
+class AcmeController extends Controller {
+
+    /**
+     * Method call by the url "/" and return a simple view.
+     *
+     * @param RequestInterface $request
+     * @param ResponseInterface $response
+     * @param array $args
+     * @return ResponseInterface
+     */
+    public function index(RequestInterface $request, ResponseInterface $response, array $args): ResponseInterface{
+        return $this->twig->render($response, '\Acme\View\index.html.twig', array());
+    }
+}
