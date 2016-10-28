046a6eafef1db1e8eb6afd6acc5396f252da9975
Alexis Merceron <merceron.alexis.dev@gmail.com>
2016-10-28 03:50:37 -0400

    Allow multiple injection

--- www/index.php
+++ www/index.php
@@ -3,6 +3,7 @@ require(dirname(__DIR__).'/vendor/autoload.php');
 
 $config = json_decode(file_get_contents('../config/config.json'), true);
 $routing = json_decode(file_get_contents('../config/routing.json'), true);
+$injection = json_decode(file_get_contents('../config/injection.json'), true);
 
 $app = new \Tit\lib\Tit();
-$app->run($config, $routing);
+$app->run($config, $routing, [$injection]);
