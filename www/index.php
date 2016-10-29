3d3ab5a6476b43eaf03018b3e59ab08b09ad1e36
Alexis Merceron <merceron.alexis.dev@gmail.com>
2016-10-29 00:50:36 -0400

    Allow multiple routing files

--- www/index.php
+++ www/index.php
@@ -6,4 +6,4 @@ $routing = json_decode(file_get_contents('../config/routing.json'), true);
 $injection = json_decode(file_get_contents('../config/injection.json'), true);
 
 $app = new \Tit\lib\Tit();
-$app->run($config, $routing, [$injection]);
+$app->run($config, [$routing], [$injection]);
